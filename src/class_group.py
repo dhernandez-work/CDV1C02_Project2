7873059c701265483fa868470b732e3fb88046fa
dhernandez-work <168514999+dhernandez-work@users.noreply.github.com>
2026-08-19 15:33:06 +0800

    style: resolve pylint warnings and improve PEP 8 compliance

--- src/class_group.py
+++ src/class_group.py
@@ -1,45 +1,56 @@
+"""This module defines the ClassGroup class."""
 from src.student import Student
 
 class ClassGroup:
+    """Represents a group of students with a maximum capacity."""
 
     def __init__(self, capacity: int):
+        """Initializes the ClassGroup with a given capacity."""
         self._capacity = capacity
         self._students = [None] * capacity
         self._size = 0
 
     def add_student(self, student: Student) -> bool:
-        if self._size >= self._capacity: return False
+        """Adds a student to the class group."""
+        if self._size >= self._capacity:
+            return False
         self._students[self._size] = student
         self._size += 1
         return True
 
     def remove_student(self, student_id: int) -> bool:
+        """Removes a student from the class group by their ID."""
         for i in range(self._size):
             if self._students[i].get_id() == student_id:
                 for j in range(i, self._size - 1):
                     self._students[j] = self._students[j + 1]
-                
+
                 self._students[self._size - 1] = None
                 self._size -= 1
                 return True
-        
+
         return False
 
     def get_the_oldest_student(self) -> Student:
-        if self._size == 0: return None
+        """Returns the oldest student in the class group."""
+        if self._size == 0:
+            return None
 
         oldest = self._students[0]
         for i in range(1, self._size):
             if self._students[i].get_birthday() < oldest.get_birthday():
                 oldest = self._students[i]
-        
+
         return oldest
 
     def get_capacity(self) -> int:
+        """Returns the maximum capacity of the class group."""
         return self._capacity
 
     def get_size(self) -> int:
+        """Returns the current number of students in the class group."""
         return self._size
 
     def get_students(self) -> list:
+        """Returns the list of students in the class group."""
         return self._students
--- src/student.py
+++ src/student.py
@@ -1,69 +1,88 @@
+"""This module defines the Student class."""
 from datetime import date
 import random
 
 class Student:
+    """Represents a student entity with their details."""
 
-    def __init__(self, id: int, name: str, birthday: date, friend: 'Student' = None):
-        self._id = id
+    def __init__(self, student_id: int, name: str, birthday: date, friend: 'Student' = None):
+        """Initializes a Student with an ID, name, birthday, and an optional best friend."""
+        self._id = student_id
         self._name = name
         self._birthday = birthday
         self._friend = friend # can be null if don't have any best friend
 
     def get_id(self) -> int:
+        """Returns the student's ID."""
         return self._id
 
     def get_name(self) -> str:
+        """Returns the student's name."""
         return self._name
 
     def get_birthday(self) -> date:
+        """Returns the student's birthday."""
         return self._birthday
 
     def get_friend(self) -> 'Student':
+        """Returns the student's best friend."""
         return self._friend
 
-    def set_id(self, id: int) -> None:
-        self._id = id
+    def set_id(self, student_id: int) -> None:
+        """Sets the student's ID."""
+        self._id = student_id
 
     def set_name(self, name: str) -> None:
+        """Sets the student's name."""
         self._name = name
 
     def set_birthday(self, birthday: date) -> None:
+        """Sets the student's birthday."""
         self._birthday = birthday
 
     def set_friend(self, friend: 'Student') -> None:
+        """Sets the student's best friend."""
         self._friend = friend
 
     def __eq__(self, o: object) -> bool:
-        if self is o: return True
-        if o is None or self.__class__ != o.__class__: return False
+        """Checks if this student is equal to another object."""
+        if self is o:
+            return True
+        if o is None or self.__class__ != o.__class__:
+            return False
         return (self._id == o.get_id() and
                 self._name == o.get_name() and
                 self._birthday == o.get_birthday())
 
     def __hash__(self) -> int:
+        """Returns the hash code for the student."""
         return hash((self._id, self._name, self._birthday))
 
     @staticmethod
     def compare_by_name(s1: 'Student', s2: 'Student') -> int:
+        """Compares two students by their names alphabetically."""
         name1 = s1.get_name().lower()
         name2 = s2.get_name().lower()
         return (name1 > name2) - (name1 < name2)
 
     @staticmethod
     def compare_by_birthday(s1: 'Student', s2: 'Student') -> int:
+        """Compares two students by their birthdays."""
         b1 = s1.get_birthday()
         b2 = s2.get_birthday()
         return (b1 > b2) - (b1 < b2)
 
     def __str__(self) -> str:
+        """Returns a string representation of the student."""
         friend_name = self._friend.get_name() if self._friend else "no best friend"
         return "Student{id = " + str(self._id) + \
                ", name = '" + self._name + '\'' + \
                ", birthday = " + str(self._birthday) + \
                ", friend = " + friend_name + \
                '}'
-    
+
     def assign_random_username(self, rand: random.Random) -> None:
+        """Assigns a random alphanumeric username to the student."""
         min_length = 5
         max_length = 10
 
@@ -72,7 +91,7 @@ class Student:
         sb = []
         chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_"
 
-        for i in range(length):
+        for _ in range(length):
             sb.append(chars[rand.randint(0, len(chars) - 1)])
 
         self._name = "".join(sb)
